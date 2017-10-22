32fc5cb05097520f9e130c9c02eb1bcdacb3410d
Alexis <>
2017-10-21 21:35:02 -0500

    Primer commit para el repositorio de prueba.

---
+++ src/grupo2/Grupo2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grupo2;
+
+/**
+ *
+ * @author fredy
+ */
+public class Grupo2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
